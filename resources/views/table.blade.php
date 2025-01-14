a3bb39ab77921fdbc421e2e756b9b2a1d3efd9c4
lmichellmendoza <mendoxa1239@gmail.com>
2025-01-13 18:03:02 -0600

    modificacion del diseño de la tabla con diseñor responsivo

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -25,9 +25,6 @@
                 </div>
             </div>
 
-
-
-
             <form>
                 <label for="search"
                     class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
@@ -47,133 +44,106 @@
                 </div>
             </form>
 
-
-
-            <table class="w-full text-sm text-left rtl:text-right text-gray-900 dark:text-gray-400 px-5 py-5">
-                <thead class="texto-lg text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr class="texto-lg text-black-700 uppercase bg-gray-100 dark:bg-gray-400 dark:text-black-400 py-2">
-                        <th class="px-6 py-3">FOLIO</th>
-                        <th class="px-6 py-3">FECHA DE SOLICITUD </th>
-                        <th class="px-6 py-3">FECHA DE REPORTE </th>
-                        <th class="px-6 py-3">ÁREA </th>
-                        <th class="px-6 py-3">SISTEMA</th>
-                        <th class="px-6 py-3">TIPO DE REPORTE</th>
-                        <th class="px-6 py-3">USUARIO QUE REPORTA </th>
-                        {{-- Cabecerea de botones --}}
-                        {{-- @role('admin') --}}
-                        <th class="px-10 py-3"></th>
-                        <th class="px-6 py-3"></th>
-                        <th class="px-6 py-3"></th>
-
-                        {{-- @endrole --}}
-                    </tr>
-                </thead>
-                <tbody>
-                    <ul>
-                        @foreach ($reporte as $reporte)
-                            <tr class=" bg-white dark:bg-white dark:text-black-400 text-sm ">
-
-                                <td class="px-6 py-3">{{ $reporte->folio }}</td>
-                                <td class="px-6 py-3">{{ $reporte->application_date }} </td>
-                                <td class="px-6 py-3">{{ $reporte->report_date }} </td>
-                                <td class="px-6 py-3">{{ $reporte->area }} </td>
-                                <td class="px-6 py-3">{{ $reporte->system }}</td>
-                                <td class="px-6 py-3">{{ $reporte->type_report }}</td>
-                                <td class="px-6 py-3">{{ $reporte->reporting_user }} </td>
-
-                                {{--  botones --}}
-
-                                {{-- Ver detalles --}}
-                                <td>
-                                    <form action="{{ route('reports.edit', $reporte->id) }}" method="GET">
-                                        @csrf
-                                        <button type="submit"
-                                            class="w-1/8 py-3 px-6 single-line inline-flex text-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700">
-                                            Verdetalles
-                                        </button>
-                                    </form>
-                                </td>
-                                {{-- <td>
-                                    <button type="button" data-id="{{ $reporte->id }}"
-                                        class="view-details w-1/8 py-3 px-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700">
-                                        Ver Detalles
-                                    </button>
-                                </td> --}}
-
-                                {{-- Eliminiar --}}
-                                <td>
-                                    <form action="{{ route('reports.destroy', $reporte->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit"
-                                            class="w-1/8 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-red-600 text-white hover:bg-red-700">
-                                            Eliminar
-                                        </button>
-                                    </form>
-                                </td>
-                                {{-- PDF --}}
-                                <td>
-                                    <form action="" method="">
-                                        @csrf
-                                        {{-- @method('DELETE') --}}
-                                        <button type="button"
-                                            class="text-orange-400 hover:text-white border border-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-orange-300 dark:text-orange-300 dark:hover:text-white dark:hover:bg-orange-400 dark:focus:ring-yellow-900">PDF</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </ul>
-                </tbody>
-            </table>
-        </div>
-
-        <!-- Modal -->
-        {{-- <div id="deleteModal" class="fixed inset-0 items-center">
-
-        </div> --}}
-
-
-
-
-
-
-        {{-- Modal ver detalles --}}
-
-        {{-- <div id="detailsModal" class="fixed inset-0 flex items-center justify-center z-50 ">
-            <div class="bg-white w-1/3 rounded-lg shadow-lg ">
-                <div class="p-4 border-b">
-                    <h3 class="text-lg font-semibold">Detalles del Reporte</h3>
-                </div>
-                <div class="p-4">
-                    <p><strong>Folio:</strong> <span id="modalFolio"></span></p>
-                    <p><strong>Fecha de Solicitud:</strong> <span id="modalApplicationDate"></span></p>
-                    <p><strong>Fecha de Reporte:</strong> <span id="modalReportDate"></span></p>
-                    <p><strong>Área:</strong> <span id="modalArea"></span></p>
-                    <p><strong>Sistema:</strong> <span id="modalSystem"></span></p>
-                    <p><strong>Tipo de Reporte:</strong> <span id="modalTypeReport"></span></p>
-                    <p><strong>Usuario que Reporta:</strong> <span id="modalReportingUser"></span></p>
-                </div>
+<div class=" mt-4 relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    FOLIO
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    FECHA DE SOLICITUD
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    FECHA DE REPORTE
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    ÁREA
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    SISTEMA
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    TIPO DE REPORTE
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    USUARIO QUE REPORTA
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+              
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($reporte as $reporte)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $reporte->folio }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $reporte->application_date }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $reporte->report_date }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $reporte->area }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $reporte->system }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $reporte->type_report }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $reporte->reporting_user }}
+                </td>
+                
+            
+            <td>
+                <form action="{{ route('reports.edit', $reporte->id) }}" method="GET">
+                    @csrf
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Verdetalles
+                    </button>
+                </form>
+            </td>
+            <td>
+                <form action="{{ route('reports.destroy', $reporte->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                        Eliminar
+                    </button>
+                </form>
+            </td>
+            {{-- PDF --}}
+            <td>
+                <form action="" method="">
+                    @csrf
+                    {{-- @method('DELETE') --}}
+                    <button type="button"
+                        class="text-orange-400 hover:text-white border border-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-orange-300 dark:text-orange-300 dark:hover:text-white dark:hover:bg-orange-400 dark:focus:ring-yellow-900">PDF</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
-                <div class="p-4 border-t flex justify-end">
-                    <button id="closeModal"
-                        class="py-2 px-4 bg-red-500 text-white rounded-lg hover:bg-red-700">Cerrar</button>
-                </div>
-            </div>
-            <div class="absolute inset-0 bg-black opacity-50"></div>
-        </div> --}}
+
+
     </body>
 
 </x-app-layout>
 
-{{-- Modal de ver detalles --}}
 
-
-
-
-
-
-
-
-
-
-{{-- Modal de eliminar --}}
