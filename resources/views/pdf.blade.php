0bc37be0de809ed6be348c20a5879f6eab428999
joelaquino12fierro <joelaquinof@gmail.com>
2025-01-15 18:04:25 -0600

    se re diseño el PDF

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -11,179 +11,120 @@
 
 
 <body>
-    {{-- Encabezado --}}
-    <header>
-        <table class="tablaHeader">
-            <thead>
+    <div class="container">
+        <header>
+            <!-- <div class="header-left">
+                <h2>AGENCIA FEDERAL DE AVIACIÓN CIVIL</h2>
+                <p>DIRECCIÓN DE DESARROLLO ESTRATÉGICO</p>
+                <p>DEPARTAMENTO DE TECNOLOGÍA INFORMÁTICA Y ATENCIÓN A USUARIOS</p>
+                <h3>SOLICITUDES DE ATENCIÓN</h3>
+            </div> -->
+            <div class="header-right">
+                <table>
+                    <!-- <tr>
+                        <td>
+                            <img src="{{ public_path('img/AFAC_color.png') }}" alt="" class="logo">
+                            {{-- Logo AFAC --}}
+                        </td>
+                    </tr> -->
+                    <tr>
+                    <td>
+                            <img src="{{ public_path('img/AFAC_color.png') }}" alt="" class="logo">
+                            {{-- Logo AFAC --}}
+                        </td>
+                        <td>Hoja</td>
+                        <td>1 de 1</td>
+                    </tr>
+                    <tr>
+                        <td>Proceso</td>
+                        <td>ADP</td>
+                    </tr>
+                    <tr>
+                        <td>Versión plantilla</td>
+                        <td>26/07/2024</td>
+                    </tr>
+                </table>
+            </div>
+        </header>
+        <div class="folio">
+            <h4>Folio: DTIARS-0108</h4>
+        </div>
+        <section class="info">
+            <table>
                 <tr>
-                    <th>
-                        <img src="{{ public_path('img/AFAC_color.png') }}" alt="" class="logo">
-                        {{-- Logo AFAC --}}
-                    </th>
-                    <th class="AFAC">
-                        AGENCIA FEDERAL DE AVIACIÓN CIVIL
-                        DIRECCIÓN DE DESARROLLO ESTRATÉGICO
-                        DEPARTAMENTO DE TECNOLOGÍA INFORMÁTICA Y ATENCIÓN A USUARIOS</p>
-                    </th>
-                    <th>
-                    <th>
-                        Hoja
-                    </th>
-                    <th>
-                        Proceso
-                    </th>
-                    <th>
-                        Versión plantilla
-                    </th>
-                    <th>
-                        Fecha plantilla
-                    </th>
-                    </th>
+                    <td><strong>Área:</strong></td>
+                    <td>UTIC</td>
+                    <td><strong>Sistema:</strong></td>
+                    <td>Sistema de Citas SICT</td>
                 </tr>
-            </thead>
-            <tbody>
-                <td>
-
-                </td>
-                <td>
-                    SOLICITUDES DE ATENCIÓN
-                </td>
-                <td>
-                <td>
-                    1 de 1
-                </td>
-                <td>
-                    ADP
-                </td>
-                <td>
-                    1.0
-                </td>
-                <td>
-                    26/07/2024
-                </td>
-                DDE-DTI
-                </td>
-            </tbody>
-        </table>
-    </header>
-
-
-
-
-
-
-    <div class="divFolio">
-        <h2 class="folio"> Folio: </h2>
+                <tr>
+                    <td><strong>Tipo de reporte:</strong></td>
+                    <td colspan="3">Solicitud</td>
+                </tr>
+                <tr>
+                    <td><strong>Fecha de entrega:</strong></td>
+                    <td>19/10/2024</td>
+                    <td><strong>Usuario que Genera el Reporte:</strong></td>
+                    <td>Oscar Manuel Cornelio Vazquez</td>
+                </tr>
+            </table>
+        </section>
+        <section class="descripcion">
+            <h4>Descripción de la solicitud:</h4>
+            <p>
+                Se solicita el apoyo con la corrección del siguiente dato, en la plataforma de citas, a continuación detallado.
+            </p>
+            <p>
+                Se concluyó con error el trámite del usuario Rolando Manuel Sanchez Díaz, ya que dice "Atendido Apto" y DEBE DECIR "ATENDIDO NO APTO".
+            </p>
+        </section>
+        <section class="solicitud">
+            <h4>SOLICITUD</h4>
+            <table>
+                <tr>
+                    <th>Módulo</th>
+                    <th>Descripción</th>
+                </tr>
+                <tr>
+                    <td>CITAS AGENDADAS</td>
+                    <td>
+                        ROLANDO MANUEL SANCHEZ DIAZ<br>
+                        CURP: SADRB60326HCNSNL07<br>
+                        ANTES: ATENDIDO APTO<br>
+                        ACTUALIZACIÓN: ATENDIDO NO APTO
+                    </td>
+                </tr>
+            </table>
+        </section>
+        <section class="usuarios">
+            <h4>USUARIOS RESPONSABLES</h4>
+            <table>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Departamento</th>
+                    <th>Cargo</th>
+                    <th>Observaciones</th>
+                </tr>
+                <tr>
+                    <td>JUAREZ JESSICA</td>
+                    <td>JEFA DE DEPARTAMENTO</td>
+                    <td>JEFA DE DEPARTAMENTO</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ANGEL CAMEZCO</td>
+                    <td>DESARROLLO</td>
+                    <td>INSPECTOR B</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>MANUEL ORTEGA</td>
+                    <td>SISTEMAS</td>
+                    <td>INSPECTOR B</td>
+                    <td></td>
+                </tr>
+            </table>
+        </section>
     </div>
-    <table>
-        <thead>
-            <tr>
-                {{-- <div class="area"> --}}
-                <th>
-                    Área:
-                </th>
-                {{-- </div> --}}
-
-                <th>
-                    Sistema:
-                </th>
-                <th>
-                    Tipo de reporte:
-                </th>
-                <th>
-                    Fecha de entrega:
-                </th>
-                <th>
-                    Usuario que genera el reporte:
-                </th>
-                <th>
-                    Descripción de la solicitud
-                </th>
-
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    UTIC
-                </td>
-                <td>
-                    Sistema de citas SICT
-                </td>
-                <td>
-                    Incidencia
-                    Falla
-                    Solicitud
-                </td>
-                <td>
-                    19/10/2024
-                </td>
-                <td>
-                    Leslie Mendoza
-                </td>
-                <td>
-                    Descripción de la solicitud
-                </td>
-
-            </tr>
-        </tbody>
-
-    </table>
-    <div class="divSolicitud">
-        <h2 class="solicitud">
-            SOLICITUD
-        </h2>
-    </div>
-
-    <table>
-        <thead>
-            <tr>
-                <th>
-                    Módulo
-                </th>
-                <th>
-                    Descripción
-                </th>
-
-            <tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    CITAS AGENDADAS
-                </td>
-                <td>
-                    Descripción
-                </td>
-
-            </tr>
-
-        </tbody>
-    </table>
-    <div class="divUsuarios">
-        <p class="usuarios">USUARIOS RESPONSABLES</p>
-    </div>
-    <table>
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Departamentod</th>
-                <th>Cargo</th>
-                <th>Observaciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Jorge Alberto Mondragón Escamilla</td>
-                <td>Desarrollo Estratégico</td>
-                <td>Insepector B</td>
-                <td> </td>
-            </tr>
-        </tbody>
-    </table>
-
-
-
 </body>
-
 </html>
