101f06dca9d94cab430bd78c6f335a6352b6e911
joelaquino12fierro <joelaquinof@gmail.com>
2025-01-16 14:44:32 -0600

    se agregaron estilos a el PDF final y se manejo la estructura

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -13,25 +13,20 @@
 <body>
     <div class="container">
         <header>
-            <!-- <div class="header-left">
-                <h2>AGENCIA FEDERAL DE AVIACIÓN CIVIL</h2>
-                <p>DIRECCIÓN DE DESARROLLO ESTRATÉGICO</p>
-                <p>DEPARTAMENTO DE TECNOLOGÍA INFORMÁTICA Y ATENCIÓN A USUARIOS</p>
-                <h3>SOLICITUDES DE ATENCIÓN</h3>
-            </div> -->
             <div class="header-right">
                 <table>
-                    <!-- <tr>
-                        <td>
-                            <img src="{{ public_path('img/AFAC_color.png') }}" alt="" class="logo">
-                            {{-- Logo AFAC --}}
-                        </td>
-                    </tr> -->
                     <tr>
-                    <td>
+                    <td rowspan="5">
                             <img src="{{ public_path('img/AFAC_color.png') }}" alt="" class="logo">
                             {{-- Logo AFAC --}}
                         </td>
+                        <td rowspan="4">
+                            <p> <strong>AGENCIA FEDERAL DE AVIACIÓN CIVIL</strong></p>
+                            <p>
+                                DEPARTAMENTO DE TECNOLOGÍA INFORMÁTICA Y ATENCIÓN A USUARIOS
+                                DIRECCIÓN DE DESARROLLO ESTRATÉGICO
+                            </p> 
+                            </td>
                         <td>Hoja</td>
                         <td>1 de 1</td>
                     </tr>
@@ -41,7 +36,15 @@
                     </tr>
                     <tr>
                         <td>Versión plantilla</td>
+                        <td>0.1</td>
+                    </tr>
+                    <tr>
+                        <td>fecha plantilla</td>
                         <td>26/07/2024</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><strong>SOLICITUDES DE ATENCIÓN</strong></td>
+                        <td>DDE-DTIyARS F01</td>
                     </tr>
                 </table>
             </div>
@@ -53,37 +56,38 @@
             <table>
                 <tr>
                     <td><strong>Área:</strong></td>
-                    <td>UTIC</td>
                     <td><strong>Sistema:</strong></td>
-                    <td>Sistema de Citas SICT</td>
-                </tr>
-                <tr>
                     <td><strong>Tipo de reporte:</strong></td>
-                    <td colspan="3">Solicitud</td>
                 </tr>
                 <tr>
                     <td><strong>Fecha de entrega:</strong></td>
-                    <td>19/10/2024</td>
                     <td><strong>Usuario que Genera el Reporte:</strong></td>
+                    <td>Solicitud</td>
+                </tr>
+                <tr>
+                    <td>19/10/2024</td>
                     <td>Oscar Manuel Cornelio Vazquez</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td colspan="3"> 
+                        Descripción de la solicitud:
+                        <p>
+                            Se solicita el apoyo con la corrección del siguiente dato, en la plataforma de citas, a continuación detallado.
+                            Se concluyó con error el trámite del usuario Rolando Manuel Sanchez Díaz, ya que dice "Atendido Apto" y DEBE DECIR "ATENDIDO NO APTO".
+                        </p>                       
+                    </td>
                 </tr>
             </table>
         </section>
-        <section class="descripcion">
-            <h4>Descripción de la solicitud:</h4>
-            <p>
-                Se solicita el apoyo con la corrección del siguiente dato, en la plataforma de citas, a continuación detallado.
-            </p>
-            <p>
-                Se concluyó con error el trámite del usuario Rolando Manuel Sanchez Díaz, ya que dice "Atendido Apto" y DEBE DECIR "ATENDIDO NO APTO".
-            </p>
-        </section>
         <section class="solicitud">
-            <h4>SOLICITUD</h4>
             <table>
                 <tr>
-                    <th>Módulo</th>
-                    <th>Descripción</th>
+                <th colspan="2"><strong>SOLICITUD</strong></th>
+                </tr>
+                <tr>
+                    <td>Módulo</td>
+                    <td>Descripción</td>
                 </tr>
                 <tr>
                     <td>CITAS AGENDADAS</td>
@@ -97,8 +101,12 @@
             </table>
         </section>
         <section class="usuarios">
-            <h4>USUARIOS RESPONSABLES</h4>
             <table>
+                <tr>
+                    <th colspan="4">
+                        <h4>USUARIOS RESPONSABLES</h4>
+                    </th>
+                </tr>
                 <tr>
                     <th>Nombre</th>
                     <th>Departamento</th>
