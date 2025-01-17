a1b1465a60dd0d0238ad2245242f9350d7c29e84
lmichellmendoza <mendoxa1239@gmail.com>
2025-01-17 13:20:14 -0600

    cambio de color de la AFAC

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
 
             <!-- Page Heading (Esto afecta las cabeceras)-->
             @if (isset($header))
-                <header class="bg-blue-900 shadow">
+                <header class="bg-azul-afac shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -14,13 +14,13 @@
                     <form action="{{ route('newform') }}">
                         @csrf
                         <button type="submit"
-                            class="w-1/8 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-sky-500 text-white hover:bg-sky-700 ">Nuevo
+                            class="w-1/8 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-700 text-white hover:bg-blue-800 ">Nuevo
                             reporte</button>
                     </form>
                 </div>
                 <div class="basic -1/7 mb-4 ">
                     <button type="button"
-                        class="w-1/8 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-green-500 text-white hover:bg-green-700 ">Exportar
+                        class="w-1/8 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-green-700 text-white hover:bg-green-800 ">Exportar
                         a Excel</button>
                 </div>
             </div>
