544c4ebb98d4a18ee0f31379871c2af1a717f141
lmichellmendoza <mendoxa1239@gmail.com>
2025-01-20 14:56:31 -0600

    no arreglo nada aún

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -36,14 +36,35 @@
                             @enderror
                         </div>
                         {{-- Área --}}
+
+
+                   {{--*
                         <div class="mb-4">
                             <label for="area" class="block text-sm font-medium text-gray-700">Área</label>
                             {{-- <input type="area" name="area" id="area" value="{{ old('area', $reporte->area) }}" --}}
-                            <select id="area"
-                                class="w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
+                            {{-- <select id="area" --}}
+                                {{-- class="w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900"> --}}
+                                {{-- <option value="{{ old('area', $reporte->areas) }}"></option> --}}
+                                {{-- <option value="desarrollo_estrategico">Desarrollo estratégico</option> --}}
+                                {{-- <option value="recursos_humanos">Recursos humanos</option> --}}
+                                {{-- <option value="ventanilla">Ventanilla</option> --}}
+                            {{-- </select> --}}
+                            {{-- class="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"> --}}
+                            {{-- @error('area') --}}
+                                {{-- <p class="text-red-500 text-xs mt-1">{{ $message }}</p> --}}
+                            {{-- @enderror --}}
+                        {{-- </div> --}}
+                   
+
+                        <div class="mb-4">
+                            <label for="area" class="block text-sm font-medium text-gray-700">Área</label>
+                            {{-- <input type="area" name="area" id="area" value="{{ old('area', $reporte->area) }}" --}}
+                            <select id="area" 
+                                class="uppercase w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
                                 {{-- <option value="" class="uppercase">--Selecciona una área--</option> --}}
-                                <option {{ old('area') == $key ? "selected" : "" }} value="{{ $value }}">
+                                
                                 @foreach ($area as $area)
+                                    <option value="old{{$area->id}}">{{old($area->areas_name)}}</option>
                                     <option class="uppercase" value="{{ $area->id }}">{{ $area->areas_name }}
                                     </option>
                                 @endforeach
@@ -55,7 +76,7 @@
                             <label for="system" class="block text-sm font-medium text-gray-700">Sistema</label>
                             {{-- <input type="system" name="system" id="system" value="{{ old('system', $reporte->system) }}"> --}}
                             <select id="system"
-                                class="w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
+                                class="uppercase w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
                                 <option value="" class="uppercase">--Selecciona un sistema--</option>
                                 @foreach ($system as $system)
                                     <option class="uppercase" value="{{ $system->id }}">{{ $system->systems_name }}
@@ -68,7 +89,7 @@
                             <label for="type_report" class="block text-sm font-medium text-gray-700">Tipo</label>
                             {{-- <input type="type_report" name="type_report" id="type_report" value="{{ old('type_report', $reporte->type_report) }}"> --}}
                             <select id="type_report"
-                                class="w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
+                                class="uppercase w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
 
                                 <option value="" class="uppercase">--Selecciona el tipo--</option>
                                 @foreach ($type as $type)
@@ -82,7 +103,7 @@
                         <div class="mb-4">
                             <label for="reporting_user" class="block text-sm font-medium text-gray-700">Usuario</label>
                             <select id="reporting_user"
-                                class="w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
+                                class="uppercase w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900">
                                 <option value="" class="uppercase">--Selecciona al usuario--</option>
                                 @foreach ($user as $user)
                                     <option class="uppercase" value="{{ $user->id }}">{{ $user->name }}</option>
