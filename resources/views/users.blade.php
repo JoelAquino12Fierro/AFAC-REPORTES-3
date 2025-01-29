7ba57fc4ec8c6d2a8fe68b12c5f47271972980bc
joelaquino12fierro <joelaquinof@gmail.com>
2025-01-29 14:07:55 -0600

    cambios joel de diseño

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -10,22 +10,26 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <body>
                     <div class="px-10 py-10"> 
+                        <div>
+                            <form>
+                                <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-gray-50 p-4 rounded-t-lg border-b border-gray-300"> 
+        
+                                    <div class="relative">
+                                        <label for="table-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Buscar</label>
+                                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                            </svg>
+                                        </div>
+                                        <input type="text" id="table-search-users" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar Usuario">
+                                    </div>
+                                    <!-- boton para el modal de nuevo usuario -->
+                                    @livewire('new-user-modal')
+                                </div>
+                            </form>
+                        </div>
                         <div class="relative overflow-x-auto shadow-md sm:rounded-lg border border-gray-200">
 
-                            <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-gray-50 p-4 rounded-t-lg border-b border-gray-300"> 
-    
-                                <div class="relative">
-                                    <label for="table-search" class="sr-only">Buscar</label>
-                                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                        </svg>
-                                    </div>
-                                    <input type="text" id="table-search-users" class="mt-4 left-2 block p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-100 focus:ring-blue-500 focus:border-blue-500 shadow-md" placeholder="Buscar Usuario">
-                                </div>
-                                <!-- boton para el modal de nuevo usuario -->
-                                @livewire('new-user-modal')
-                            </div>
 
                             <table class="w-full text-sm text-left text-gray-700" id="user-table">
                                 <thead class="text-xs text-black uppercase">
@@ -43,9 +47,9 @@
                                         <td class="px-6 py-4 text-gray-900 font-medium">
                                             {{ $user->id }}
                                         </td>
-                                        <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
+                                        <th scope="row" class="px-6 py-4 text-gray-900 whitespace-nowrap">
                                             <div class="ps-3">
-                                                <div class="text-lg font-semibold text-gray-700">
+                                                <div class="text-base font-semibold text-gray-700">
                                                     {{ $user->name }}
                                                 </div>
                                                 <div class="font-normal text-gray-500">
