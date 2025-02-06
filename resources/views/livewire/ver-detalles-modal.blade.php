00707bc1c68a91cf3ea3946ca658751e4cc0b820
joelaquino12fierro <joelaquinof@gmail.com>
2025-02-06 14:07:01 -0600

    cambio en los botones de la vista table

--- resources/views/livewire/ver-detalles-modal.blade.php
+++ resources/views/livewire/ver-detalles-modal.blade.php
@@ -1,26 +1,3 @@
 <div>
-    {{-- MODAL PARA VER LOS DETALLES DE LA TABLA DE REGISTROS. --}}
-
-    <x-button class="text-left font-medium text-blue-600 hover:underline">
-        Ver detalles
-    </x-button>
-
-    <x-dialog-modal>
-        <x-slot name="title">
-
-        </x-slot>
-
-        <x-slot name="content">
-            <div>
-                
-            </div>
-        </x-slot>
-
-        <x-slot name="footer">
-            <div>
-                <x-button>Cancelar</x-button>
-                <x-button>guardar</x-button>
-            </div>
-        </x-slot>
-    </x-dialog-modal>
+    {{-- The whole world belongs to you. --}}
 </div>
